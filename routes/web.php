e1fac82755e328d08e276275c60c468d38f43157
Curso Git <marcooperdomo@gmail.com>
2019-08-05 12:26:41 -0500

    Mostrando los posts desde la BD

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,13 @@
 |
 */
 
+// Ruta por defecto
 Route::get('/', function () {
-    return view('welcome');
+    $posts = App\Post::all();//Lama a todos los posts
+    $posts = App\Post::latest('published_at')->get();//Lama a todos los posts de forma descendente
+    return view('welcome', compact('posts'));
+});
+// Ruta de prueba
+Route::get('posts', function () {
+    return App\Post::all();
 });
