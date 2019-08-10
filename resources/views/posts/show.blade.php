90dc0c05c40713e4e6c0215b3bc32206965319c4
Curso Git <marcooperdomo@gmail.com>
2019-08-09 23:50:53 -0500

    Galería de fotos

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,6 +7,8 @@
   <article class="post container">
     @if ($post->photos->count() === 1)
         <figure><img src="{{ $post->photos->first()->url }}" alt="" class="img-responsive"></figure>
+    @elseif($post->photos->count() > 1)
+      @include('posts.carousel')
     @endif
     <div class="content-post">
       <header class="container-flex space-between">
@@ -40,7 +42,21 @@
   </article>
 @endsection
 
+@push('styles')
+  {{-- Bootstrap CSS Customize 3.4 --}}
+    {{-- No se puede customizar la versión 3.3.7 --}}
+    <link rel="stylesheet" href="/css/twitter-bootstrap.css">
+@endpush
 @push('scripts')
   {{-- Disqus js --}}
     <script id="dsq-count-scr" src="//zendero.disqus.com/count.js" async></script>
+  {{-- JQuery 3.2.1 --}}
+    <script
+			  src="https://code.jquery.com/jquery-3.2.1.min.js"
+			  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+        crossorigin="anonymous">
+    </script>
+  {{-- Bootstrap JS Customize 3.4 --}}
+    {{-- No se puede customizar la versión 3.3.7 --}}
+    <script src="/js/twitter-bootstrap.js"></script>
 @endpush
