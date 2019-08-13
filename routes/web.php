939d6a5ce257fdefb034e4f26a5ccd3a41ce582a
Curso Git <marcooperdomo@gmail.com>
2019-08-13 00:16:08 -0500

    Activando la navegación

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 // Ruta por defecto
-Route::get('/', 'PagesController@home');
+Route::get('/', 'PagesController@home')->name('pages.home');
+Route::get('nosotros', 'PagesController@about')->name('pages.about');
+Route::get('archivo', 'PagesController@archive')->name('pages.archive');
+Route::get('contacto', 'PagesController@contact')->name('pages.contact');
+
 Route::get('blog/{post}', 'PostsController@show')->name('posts.show');
 Route::get('categories/{category}', 'CategoriesController@show')->name('categories.show');
 Route::get('tags/{tag}', 'TagsController@show')->name('tags.show');
