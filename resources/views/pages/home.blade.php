41cd5442d89ae6e2b80795fc433b578f47116fb8
Curso Git <marcooperdomo@gmail.com>
2019-08-13 15:02:20 -0500

    Reestructuración de vistas

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,48 +8,32 @@
 		@foreach ($posts as $post)
 				<article class="post">
 					@if ($post->photos->count() === 1)
-							<figure><img src="{{ $post->photos->first()->url }}" alt="" class="img-responsive"></figure>
+
+							@include('posts.photo')
+							
 					@elseif($post->photos->count() > 1)
-						<div class="gallery-photos masonry">
-							@foreach ($post->photos->take(4) as $photo)
-								<figure class="gallery-image">
-									@if ($loop->iteration === 4)
-											<div class="overlay">{{ $post->photos->count() }} Fotos</div>
-									@endif
-									<img src="{{ url($photo->url) }}" alt="">
-								</figure>
-							@endforeach
-						</div>
-					@elseif($post->iframe)
-						<div class="video">
-							{!! $post->iframe !!}
-						</div>
-					@endif
 					
+						@include('posts.carousel-preview')
+
+					@elseif($post->iframe)
+
+						@include('posts.iframe')
+						
+					@endif
 					<div class="content-post">
-						<header class="container-flex space-between">
-							<div class="date">
-								<span class="c-gray-1">{{ $post->published_at->format('M d') }}</span>
-								<span class="c-gray-1">- {{ $post->published_at->diffForHumans() }} / {{ $post->owner->name }}</span>
-							</div>
-							<div class="post-category">
-								<span class="category text-capitalize">
-									<a href="{{ route('categories.show', $post->category) }}">{{ $post->category->name }}</a>
-								</span>
-							</div>
-						</header>
+						
+						@include('posts.header')
+
 						<h1>{{ $post->title }}</h1>
 						<div class="divider"></div>
 						<p>{{ $post->excerpt }}</p>
 						<footer class="container-flex space-between">
 							<div class="read-more">
-								<a href="blog/{{ $post->url }}" class="text-uppercase c-green">Leer más</a>
+								<a href="{{ route('posts.show', $post) }}" class="text-uppercase c-green">Leer más</a>
 							</div>
-							<div class="tags container-flex">
-								@foreach ($post->tags as $tag)
-									<span class="tag c-gris text-capitalize"><a href="{{ route('tags.show', $tag) }}">#{{ $tag->name }}</a></span>	
-								@endforeach
-							</div>
+							
+							@include('posts.tags')
+
 						</footer>
 					</div>
 				</article>
---
+++ resources/views/posts/photo.blade.php
@@ -0,0 +1,5 @@
+<figure>
+  <img src="{{ $post->photos->first()->url }}" 
+       alt="Imagen: {{ $post->title }}" 
+       class="img-responsive">
+</figure>
