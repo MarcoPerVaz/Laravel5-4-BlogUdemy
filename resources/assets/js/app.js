964f66ac538a05a1973431cc4cfc7f4d07479956
Curso Git <marcooperdomo@gmail.com>
2019-08-21 11:19:30 -0500

    Instalación de Vue-Router

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,6 +2,10 @@
 
 window.Vue = require('vue');
 
+import Router from 'vue-router';
+
+Vue.use(Router);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
